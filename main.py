c05914ae436ac20411641eb4696e39094fe843be
Pranathi Arigela <apranathi2006@gmail.com>
2025-12-09 16:41:11 +0530

    Final project completed: full pipeline, model, app, and documentation

---
+++ main.py
@@ -0,0 +1,6 @@
+from src.pipeline.training_pipeline import TrainingPipeline
+
+if __name__ == "__main__":
+    pipeline = TrainingPipeline()
+    pipeline.run_pipeline()
+    print("Training pipeline executed successfully.")
